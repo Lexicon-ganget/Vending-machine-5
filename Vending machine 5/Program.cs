64ef55263aee2d7473d03dba6d1d663fd3e1e450
Snapz01 <timmy.larsson@hotmail.com>
2024-05-22 13:31:42 +0200

    removed product show menu and added it to purchase menu

--- Vending machine 5/Program.cs
+++ Vending machine 5/Program.cs
@@ -20,10 +20,9 @@ namespace Vending_machine_5
             Console.WriteLine("Vending Machine");
             Console.WriteLine("Enter the selection you want to");
             Console.WriteLine();
-            Console.WriteLine("Press 1 to show products");
-            Console.WriteLine("Press 2 to insert money");
-            Console.WriteLine("Press 3 to buy a product");
-            Console.WriteLine("Press 4 to end transaction and get change");
+            Console.WriteLine("Press 1 to instert money");
+            Console.WriteLine("Press 2 to buy a product");
+            Console.WriteLine("Press 3 to end transaction and get change");
             Console.WriteLine();
             Console.WriteLine("Press 9 to exit");
             Console.Write("\r\nSelect an option: ");
@@ -32,15 +31,12 @@ namespace Vending_machine_5
             switch (Console.ReadLine())
             {
                 case "1":
-                    ShowProducts();
-                    break;
-                case "2":
                     InsertMoney();
                     break;
-                case "3":
+                case "2":
                     Purchase();
                     break;
-                case "4":
+                case "3":
                     EndTransmition();
                     break;
                 case "9":
@@ -49,19 +45,6 @@ namespace Vending_machine_5
                     break;
             }
             return true;
-        }
-
-        static void ShowProducts()
-        {
-            // Show all products
-            var vendingMachine = new VendingMachineService();
-            Console.WriteLine("Available products:");
-            foreach (var product in vendingMachine.ShowAll())
-            {
-                Console.WriteLine(product);
-            }
-            Console.WriteLine("Press any key to return to menu...");
-            Console.ReadKey();
         }
 
         static void InsertMoney()
@@ -77,11 +60,31 @@ namespace Vending_machine_5
 
         static void Purchase()
         {
-            // Purchase a product
-            var vendingMachine = new VendingMachineService();
-            Console.WriteLine("\nPurchasing product with Id 1:");
-            var product1 = vendingMachine.Purchase(1);
-            Console.WriteLine(product1.Use());
+            // Purchase a product and show products
+            Console.Clear();
+            Console.WriteLine("Available products:");
+            foreach (var product in vendingMachine.ShowAll())
+            {
+                Console.WriteLine(product);
+            }
+            Console.WriteLine("Enter the product Id you want to purchase:");
+            if (int.TryParse(Console.ReadLine(), out int productId))
+            {
+                try
+                {
+                    var product = vendingMachine.Purchase(productId);
+                    Console.WriteLine($"Purchased: {product.Name}");
+                    Console.WriteLine(product.Use());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a numeric value.");
+            }
             Console.WriteLine("Press any key to return to menu...");
             Console.ReadKey();
         }
